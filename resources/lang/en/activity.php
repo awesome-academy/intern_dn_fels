bb73317d89dde555b622ea5b2b5f4cba0cb0e2b6
vunn-1010 <nguyen.nhu.vu@sun-asterisk.com>
2020-06-18 10:49:22 +0700

    Course enrollment

--- resources/lang/en/activity.php
+++ resources/lang/en/activity.php
@@ -4,6 +4,7 @@ return [
     'followUser' => ':user has followed :target',
     'unfollowUser' => ':user has unfollowed :target',
     'enrollCourse' => ':user has enrolled course :target',
+    'leaveCourse' => ':user has left course :target',
     'startLesson' => ':user has started lesson :target',
     'finishLesson' => ':user has finished lesson :target',
     'learnWord' => ':user has learnt the word :target',
